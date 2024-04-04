6f6f4d62d2558840b577e1fa2ec95461f1136d7e
diogo <diogo.github05@gmail.com>
2024-04-04 04:06:59 +0100

    felix helping format the page

---
+++ Keybind.js
@@ -0,0 +1,12 @@
+document.addEventListener('keydown', function(event) {
+    if (event.ctrlKey && event.key === 'f') {
+      event.preventDefault();
+      window.open("https://youtu.be/r11mz5jvHaU?si=HE0hqltBLV3N9SXn")
+  
+    }
+
+    if(event.ctrlKey && event.key === 'h') {
+        event.preventDefault();
+        window.location.href= ("index.html")
+    }
+  });
